94269b5c09d6141f44273a92bfb09fc9b846da49
Joe Stein <joe.stein@stealth.ly>
2013-12-18 14:18:05 -0500

    new test case to make sure a consumer from another group id can read the stream

--- src/test/scala/StubSpec.scala
+++ src/test/scala/StubSpec.scala
@@ -25,11 +25,12 @@ import kafka.utils._
 
 class KafkaSpec extends Specification with Logging {
 	
-  val testMessage = UUID.randomUUID().toString
-  val testTopic = UUID.randomUUID().toString
 
   "Sample" should {
     "send string to broker and consume that string back" in {
+      val testMessage = UUID.randomUUID().toString
+      val testTopic = UUID.randomUUID().toString
+      val groupId_1 = UUID.randomUUID().toString
 
       var testStatus = false
 
@@ -37,8 +38,7 @@ class KafkaSpec extends Specification with Logging {
       val producer = new KafkaProducer(testTopic,"192.168.86.10:9092")
       producer.sendString(testMessage)
 
-
-      val consumer = new KafkaConsumer(testTopic,"groupId","192.168.86.5:2181")
+      val consumer = new KafkaConsumer(testTopic,groupId_1,"192.168.86.5:2181")
 
       def exec(binaryObject: Array[Byte]) = {
         val message = new String(binaryObject)
@@ -54,5 +54,49 @@ class KafkaSpec extends Specification with Logging {
       
       testStatus must beTrue // we need to get to this point but a failure in exec will fail the test
     }
+
+    "send string to broker and consume that string back in different consumer groups" in {
+      val testMessage = UUID.randomUUID().toString
+      val testTopic = UUID.randomUUID().toString
+      val groupId_1 = UUID.randomUUID().toString
+      val groupId_2 = UUID.randomUUID().toString
+
+      var testStatus1 = false
+      var testStatus2 = false
+
+      info("starting sample broker testing")
+      val producer = new KafkaProducer(testTopic,"192.168.86.10:9092")
+      producer.sendString(testMessage)
+
+      val consumer1 = new KafkaConsumer(testTopic,groupId_1,"192.168.86.5:2181")
+
+      def exec1(binaryObject: Array[Byte]) = {
+        val message1 = new String(binaryObject)
+        info("testMessage 1 = " + testMessage + " and consumed message 1 = " + message1)
+        testMessage must_== message1
+        consumer1.close()
+        testStatus1 = true
+      }
+
+      info("KafkaSpec : consumer 1 - is waiting some seconds")
+      consumer1.read(exec1)
+      info("KafkaSpec : consumer 1 - consumed")
+
+      val consumer2 = new KafkaConsumer(testTopic,groupId_2,"192.168.86.5:2181")
+
+      def exec2(binaryObject: Array[Byte]) = {
+        val message2 = new String(binaryObject)
+        info("testMessage 2 = " + testMessage + " and consumed message 2 = " + message2)
+        testMessage must_== message2
+        consumer2.close()
+        testStatus2 = true
+      }
+
+      info("KafkaSpec : consumer 2 - is waiting some seconds")
+      consumer2.read(exec2)
+      info("KafkaSpec : consumer 2 - consumed")      
+      
+      testStatus2 must beTrue // we need to get to this point but a failure in exec will fail the test
+    }    
   }
 }
