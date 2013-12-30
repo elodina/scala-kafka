24a7e1e8409613826eb26a4cd6536efcf6136347
Joe Stein <joe.stein@stealth.ly>
2013-12-30 16:50:52 -0500

    fixed the repo and bash cleanup

--- vagrant/kafka.sh
+++ vagrant/kafka.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#!/bin/sh -Eu
+#!/bin/sh -Eux
 
 #  Trap non-normal exit signals: 1/HUP, 2/INT, 3/QUIT, 15/TERM, ERR
 trap founderror 1 2 3 15 ERR
@@ -30,23 +30,12 @@ exitscript()
         exit 0
 }
 
-echo "cd /tmp"
 cd /tmp
 
-archive="kafka_2.8.0-0.8.0.1_KAFKA-1180v2"
-
-#echo "wget https://archive.apache.org/dist/kafka/0.8.0/kafka_2.8.0-0.8.0.tar.gz"
-#wget https://archive.apache.org/dist/kafka/0.8.0/kafka_2.8.0-0.8.0.tar.gz
-echo "wget http://people.apache.org/~joestein/KAFKA-1180/$archive.tar.gz"
-wget http://people.apache.org/~joestein/KAFKA-1180/$archive.tar.gz
-
-echo "tar -xvf $archive.tar.gz"
+archive="kafka_2.8.0-0.8.0"
+wget https://archive.apache.org/dist/kafka/0.8.0/$archive.tar.gz
 tar -xvf $archive.tar.gz
-
-echo "mkdir -p /opt/apache"
 mkdir -p /opt/apache
-
-echo "mv $archive /opt/apache/kafka"
 mv $archive /opt/apache/kafka
 
 exitscript
