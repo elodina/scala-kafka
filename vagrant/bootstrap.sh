2ae903e6d52b29af4a0cc391b7baff1dc31ba97b
Joe Stein <joe.stein@stealth.ly>
2014-07-22 21:44:07 -0400

    make it easier to run with a curl

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -31,17 +31,25 @@ exitscript()
 }
 
 apt-get -y update
-apt-get install -y software-properties-common python-software-properties curl wget git screen
+apt-get install -y software-properties-common python-software-properties
 add-apt-repository -y ppa:webupd8team/java
 apt-get -y update
 /bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
-apt-get -y install oracle-java7-installer oracle-java7-set-default
+apt-get -y install oracle-java6-installer oracle-java6-set-default
 
-chmod a+rw -R /opt
-cd /
-ln -s /opt/stealthly/vagrant vagrant
+chmod a+rw /opt
+cd /opt
+ln -s /vagrant kafka
+cd kafka
+IP=$(ifconfig  | grep 'inet addr:'| grep 168 | grep 192|cut -d: -f2 | awk '{ print $1}')
+sed 's/broker.id=0/'broker.id=$1'/' /opt/kafka/config/server.properties > /tmp/prop1.tmp
+sed 's/#advertised.host.name=<hostname routable by clients>/'advertised.host.name=$IP'/' /tmp/prop1.tmp > /tmp/prop2.tmp
+sed 's/#host.name=localhost/'host.name=$IP'/' /tmp/prop2.tmp > /tmp/prop3.tmp
+sed 's/zookeeper.connect=localhost:2181/'zookeeper.connect=192.168.86.5:2181'/' /tmp/prop3.tmp > /opt/server.properties
 
 /vagrant/vagrant/kafka.sh #install kafka
 /vagrant/vagrant/kafkacat.sh #install the kafkacat utility
+
+/opt/apache/kafka/bin/kafka-server-start.sh /opt/server.properties 1>> /tmp/broker.log 2>> /tmp/broker.log &
 
 exitscript
