e089c6ba7caae094bf0adef5e207c6a4eb141735
Joe Stein <joe.stein@stealth.ly>
2014-07-26 14:18:26 -0400

    jdk7

--- vagrant/broker.sh
+++ vagrant/broker.sh
@@ -35,7 +35,7 @@ apt-get install -y software-properties-common python-software-properties
 add-apt-repository -y ppa:webupd8team/java
 apt-get -y update
 /bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
-apt-get -y install oracle-java6-installer oracle-java6-set-default
+apt-get -y install oracle-java7-installer oracle-java7-set-default
 
 chmod a+rw /opt
 cd /opt
