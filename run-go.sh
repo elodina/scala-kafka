dc91a1e5c9be0ee98d4c8b13b9119336bdf3484d
serejja <serejja@gmail.com>
2014-10-24 15:28:53 +0300

    switched run-go to master branch

--- run-go.sh
+++ run-go.sh
@@ -2,14 +2,6 @@
 
 mkdir -p $GOPATH/src/github.com/stealthly
 cd $GOPATH/src/github.com/stealthly
-
-#this is awful but there is no way to go get a specific branch :(
-#fetch with dependencies
 go get github.com/stealthly/go-kafka
-go get github.com/stealthly/go-avro/decoder
-rm -rf go-kafka
-
-#switch to our branch
-git clone https://github.com/stealthly/go-kafka.git --branch BDOS-6
 cd $GOPATH/src/github.com/stealthly/go-kafka
 go run scala_go_kafka.go $1 $2
